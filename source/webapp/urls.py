672ac16aae8144682f9107a0afcef5cd709966e0
Adlet Kurentaev <kurentaev@gmail.com>
2022-09-27 16:15:13 +0600

    Изменен файл urls

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,14 +1,20 @@
 from django.urls import path
 
-from webapp.views.tasks import add_view
+from webapp.views.products import add_product_view
+from webapp.views.products import add_category_view
 from webapp.views.base import index_view
-from webapp.views.tasks import task_view
-from webapp.views.tasks import delete_view
+from webapp.views.products import product_view
+from webapp.views.products import category_view
+from webapp.views.products import delete_view
+
 
 urlpatterns = [
     path('', index_view, name='index'),
-    path('todo_list/add/', add_view, name='todo_add'),
-    path('todo_list/', index_view),
-    path('todo_list/<int:pk>', task_view, name='todo_detail'),
-    path('delete_task/<int:pk>', delete_view, name='delete')
+    path('products', index_view, name='index'),
+    path('product_list/add/', add_product_view, name='product_add'),
+    path('category_list/add/', add_category_view, name='category_add'),
+    path('product_list/', index_view),
+    path('product_list/<int:pk>', product_view, name='product_detail'),
+    path('category_list/<int:pk>', category_view, name='category_detail'),
+    path('delete_product/<int:pk>', delete_view, name='delete')
 ]
