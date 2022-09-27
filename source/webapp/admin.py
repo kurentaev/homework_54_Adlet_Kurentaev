ed7a9a0a0ad1e188ae4986f73963878ab19d2f69
Adlet Kurentaev <kurentaev@gmail.com>
2022-09-27 16:13:42 +0600

    Изменен файл админ

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from webapp.models import Products
+from webapp.models import Categories
+
+
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description', 'category', 'created_at', 'price', 'image']
+    list_filter = ['id', 'title', 'description', 'category', 'created_at', 'price', 'image']
+    search_fields = ['title', 'category']
+    fields = ['id', 'title', 'description', 'category', 'created_at', 'price', 'image']
+    readonly_fields = ['id', 'created_at']
+
+
+admin.site.register(Products, ProductsAdmin)
+
+
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = ['id', 'category', 'description']
+    list_filter = ['id', 'category', 'description']
+    search_fields = ['category']
+    fields = ['id', 'category', 'description']
+    readonly_fields = ['id']
+
+
+admin.site.register(Categories, CategoriesAdmin)
+
